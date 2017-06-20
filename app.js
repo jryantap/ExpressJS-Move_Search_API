e56a8042be2a5ffb97843d2b88446ebad160be84
Jherico-Ryan Tapang <jhericoryan.tapang@gmail.com>
2017-06-20 17:17:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    res.render("search");
-});
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get("/", function(req, res){
+//    res.render("search");
+//});
 
 //parse JSON from api and store them in a var data 
 app.get("/results", function(req, res){
@@ -22,7 +26,7 @@ app.get("/results", function(req, res){
     
 });
 
-//start server for c9.io
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Movie app started!");
+//start server for heroku
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
